6798f74b7b2aea06d9c132427c026cd709807bbb
paskomartin <paskomartin@gmail.com>
2017-12-03 21:10:11 +0100

    simple gui

--- objects/gamegui.lua
+++ objects/gamegui.lua
@@ -5,6 +5,7 @@ function GameGui:new()
   gameGui.points = 0
   gameGui.coins = 0
   gameGui.playerLife = 0
+  gameGui.level = 1
   gameGui.x = 0
   gameGui.y = 17 * 32
   gameGui.h = 56
@@ -28,9 +29,16 @@ function GameGui:new()
     local text = "Coins: " .. self.coins
     local margin = 6
     love.graphics.setColor(255,255,255)
-    love.graphics.print(text, self.x + 16, self.y + 4, 0, 2, 2)
+    love.graphics.print(text, self.x + 16, self.y + 2, 0, 2, 2)
     text = "Points: " .. self.points
     love.graphics.print(text, self.x + 16, self.y + 16 + margin * 2, 0, 2, 2)
+    
+    text = "Life: " .. self.playerLife
+    local x = self.w / 2
+    love.graphics.print(text, x, self.y + 2, 0, 2, 2)
+    text = "Level: " .. self.level
+    love.graphics.print(text, x, self.y + 16 + margin * 2, 0, 2, 2)
+    
     love.graphics.setColor(255,255,255)
   end
   
